5b5f7e334dee29fbe3b0827acb3f3f21c50c3695
sid732 <siddharth.nashikkar@yahoo.com>
2021-01-25 15:23:17 +0530

    Implement mul Fn

--- math.py
+++ math.py
@@ -1,10 +1,8 @@
 # Perform addition
 def add(x,y):
-    return x+y
+   
 # Perform subtraction
 def subtract(x,y):
-    if x<y:
-        return ERROR
     return x-y
 # Perform multiplication
 def multiply(x,y):
@@ -12,9 +10,4 @@ def multiply(x,y):
     
 # Perform division
 def divide(x,y):
-    if y==0:
-        return DIVIDE_BY_ZERO
-    return x/y
-    
-def square(x):
-    return x*x
+    pass
